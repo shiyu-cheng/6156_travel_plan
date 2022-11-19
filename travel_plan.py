c3f068e37b3ce1ac77d9e0d1ad1dcc89b4e1ff59
MuDaDesGaArt <jy3166@columbia.edu>
2022-11-19 04:47:26 -0500

    sprint1 works locally

--- travel_plan.py
+++ travel_plan.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -24,13 +24,39 @@ def main_page():
     print(plan_list)
     return render_template('main_page.html', plan_list=plan_list)
 
+@app.route("/add", methods=["POST"])
+def add():
+    # add new travel plan 
+    start = request.form.get("travel_start_time")
+    end = request.form.get("travel_end_time")
+    loc = request.form.get("travel_location")
+    new_plan = TravelPlan(travel_start_time=start, travel_end_time=end, travel_location=loc, plan_completed=False)
+    db.session.add(new_plan)
+    db.session.commit()
+    return redirect(url_for("main_page"))
+
+@app.route("/update/<int:plan_id>")
+def update(plan_id):
+    # add new travel plan 
+    target_plan = TravelPlan.query.filter_by(id=plan_id).first()
+    target_plan.plan_completed = not target_plan.plan_completed
+    db.session.commit()
+    return redirect(url_for("main_page"))
+
+@app.route("/delete/<int:plan_id>")
+def delete(plan_id):
+    # add new travel plan 
+    target_plan = TravelPlan.query.filter_by(id=plan_id).first()
+    db.session.delete(target_plan)
+    db.session.commit()
+    return redirect(url_for("main_page"))
 
 if __name__ == "__main__":
     db.create_all()
 
-    new_travel_plan = TravelPlan(travel_start_time="2022/09/26", travel_end_time="2023/09/26", travel_location="Columbia University", plan_completed=False)
-    db.session.add(new_travel_plan)
-    db.session.commit()
+    # new_travel_plan = TravelPlan(travel_start_time="2022/09/26", travel_end_time="2023/09/26", travel_location="Columbia University", plan_completed=False)
+    # db.session.add(new_travel_plan)
+    # db.session.commit()
 
 
     app.run(debug=True)
