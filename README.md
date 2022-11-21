c53351b9310dd097dd1500f9b34a2463d9be2cd7
shiyu <shiyuchg@gmail.com>
2022-11-20 23:46:02 -0500

    mysql

--- README.md
+++ README.md
@@ -2,5 +2,3 @@
 EB Microservice of the 6156 groupwork, with a database for travel plan, made by Jiayi Yang and Shiyu Cheng.
 
 [Database schema](db/db_schema.png)
-
-test
--- travel_plan.py
+++ travel_plan.py
@@ -1,13 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+user = 'root'
+password = 'dbuserdbuser'
+port = '3306'
+host = 'localhost'
+db_name = '6156_travel_plan'
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+print('mysql+pymysql://%s:%s@%s:%s/%s'%(user, password, host, port, db_name))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://%s:%s@%s:%s/%s'%(user, password, host, port, db_name)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 app.app_context().push()
-
+# conn = None
 
 class TravelPlan(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,10 +25,9 @@ class TravelPlan(db.Model):
     travel_location = db.Column(db.String(100))
     plan_completed = db.Column(db.Boolean)
 
-
 @app.route('/')
 def main_page():
-    # we want to show all travel plan on main page 
+    # we want to show all travel plan on main page
     plan_list = TravelPlan.query.all()
     print(plan_list)
     return render_template('main_page.html', plan_list=plan_list)
@@ -52,11 +60,11 @@ def delete(plan_id):
     return redirect(url_for("main_page"))
 
 if __name__ == "__main__":
+    #db.drop_all()
     db.create_all()
 
     # new_travel_plan = TravelPlan(travel_start_time="2022/09/26", travel_end_time="2023/09/26", travel_location="Columbia University", plan_completed=False)
     # db.session.add(new_travel_plan)
     # db.session.commit()
-
 
     app.run(debug=True)
